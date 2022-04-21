24c2b9cc563e7e3b6df6b8b423d35b22cad0a49d
Ilder Velez <velez.ilder@gmail.com>
2022-04-20 22:05:23 -0400

    add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/search" element={<h1>Search</h1>} />

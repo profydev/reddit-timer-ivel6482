69fd7657762b5854ad73d46d5aef58d7346ff6d4
Ilder Velez <velez.ilder@gmail.com>
2022-04-20 21:31:48 -0400

    add routes for home and search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <h1>Reddit Timer</h1>
+      <Routes>
+        <Route path="/" />
+        <Route path="/search" />
+      </Routes>
+    </>
   );
 }
 

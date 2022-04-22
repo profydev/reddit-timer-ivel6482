03012d21391a7027f4e61425ca755cebf10f9f06
Ilder Vélez <38200521+ivel6482@users.noreply.github.com>
2022-04-22 01:28:59 +0000

    Create app skeleton (#1)
    * add routes for home and search
    
    * install bitter and montserrat fonts
    
    * add normalize.css
    
    * fix App component not rendering
    
    * use yarn
    
    * update global styles
    
    * update project structure
    
    * update description meta tag
    
    * update names in manifest
    
    * add favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/search" element={<h1>Search</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>404 - Page not found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './normalize.css';
+import './global.css';
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension

c94d2be38a558658a5ed67fbb8150251bab54572
Ilder Velez <velez.ilder@gmail.com>
2022-04-21 21:14:32 -0400

    update project structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
           <Route path="/search" element={<h1>Search</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>404 - Page not found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
